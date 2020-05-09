7358e4bcf1e582a4f018c80f24ac6e99d9c32fe4
Tim Speelman <timspeelman@live.nl>
2020-05-09 20:12:46 +0200

    Remove recipe client

--- src/examples/recipe-client/config.ts
+++
@@ -1,8 +0,0 @@
-import { loadTemporaryIPv8Configuration } from "../../util/ipv8conf"
-
-const client = loadTemporaryIPv8Configuration("test-alice")
-
-export const clientPeer = {
-    ipv8_url: `http://localhost:${client.port}`,
-    mid_b64: client.mid_b64,
-}
