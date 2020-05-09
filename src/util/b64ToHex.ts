21691f3863522f874807a64e9e3e34ad0fbef7aa
Tim Speelman <timspeelman@live.nl>
2020-05-09 17:11:06 +0200

    Fix

--- src/util/b64ToHex.ts
+++ src/util/b64ToHex.ts
@@ -1,5 +1,11 @@
 import atob from "atob";
 
-export function b64ToHex(b64) {
-    return atob(b64).split("").map(c => c.charCodeAt(0).toString(16)).join("")
+export function b64ToHex(str) {
+    const raw = atob(str);
+    let result = '';
+    for (let i = 0; i < raw.length; i++) {
+        const hex = raw.charCodeAt(i).toString(16);
+        result += (hex.length === 2 ? hex : '0' + hex);
+    }
+    return result;
 }
