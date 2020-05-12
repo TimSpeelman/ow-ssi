12118e52d44d50831615492875c6d4dc0d7855da
Tim Speelman <timspeelman@live.nl>
2020-05-12 15:03:55 +0200

    Add python setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OpenWalletTS',
+    version='0.1dev',
+    packages=find_packages(),
+    license='GNU Lesser General Public License'
+)
